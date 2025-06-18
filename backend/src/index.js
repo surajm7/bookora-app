c6942062215f8812d366840a68860057b9e16076
Suraj Gupta <guptasurajm07@gmail.com>
2025-06-18 18:21:31 +0530

    add  models, routes, and middleware setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,23 @@
 import express from "express";
-import dotenv from "dotenv/config";
-import {connectDB} from "./lib/db.js";
+import cors from "cors";
+import "dotenv/config";
 
-const app=express();
-const PORT=process.env.PORT || 3001;
-app.get("/",(req,res)=>{
-     res.send("Hello World");
+import authRoutes from "./routes/authRoutes.js";
+import bookRoutes from "./routes/bookRoutes.js";
+
+import { connectDB } from "./lib/db.js";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/books", bookRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+  connectDB();
 });
 
-    app.listen(PORT,()=>{
-console.log(`Server is running on port ${PORT}`)
-connectDB();
-})
